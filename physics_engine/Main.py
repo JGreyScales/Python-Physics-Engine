b00b21e2b88cf86b132474b639daae7f2d1bde6e
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-09-23 16:28:02 -0400

    REDID WHOLE WORK

--- physics_engine/Main.py
+++ physics_engine/Main.py
@@ -1,52 +1,47 @@
-
-import pygame
-import random
-  
-# GLOBAL VARIABLES
-WIDTH = 500
-HEIGHT = 500
-  
-# Object class
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, color, height, width):
-        super().__init__()
-  
-        self.image = pygame.Surface([width, height])
-        self.image.fill(128,128,128)
-        self.image.set_colorkey(255,255,255)
-  
-        pygame.draw.rect(self.image,color,pygame.Rect(0, 0, width, height))
-  
-        self.rect = self.image.get_rect()
-  
-  
+import pygame, sys, random
+from pygame.locals import *
+# Set up pygame.
 pygame.init()
-
-  
-size = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Creating Sprite")
-  
-all_sprites_list = pygame.sprite.Group()
-  
-object_ = Sprite((0, 0, 0), 20, 30)
-object_.rect.x = 200
-object_.rect.y = 300
-  
-all_sprites_list.add(object_)
-  
-looping = True
-clock = pygame.time.Clock()
-  
-while looping:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            looping = False
-  
-    all_sprites_list.update()
-    screen.fill(128,128,128)
-    all_sprites_list.draw(screen)
-    pygame.display.flip()
-    clock.tick(60)
-  
-pygame.quit()
+mainClock = pygame.time.Clock()
+ 
+# Set up the window.
+WINDOWWIDTH = 700
+WINDOWHEIGHT = 500
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Physics engine')
+ 
+ 
+ 
+def movements():
+    player = pygame.Rect(WINDOWWIDTH/2, WINDOWHEIGHT/2, 20, 20)
+ 
+    moveLeft = False
+    moveRight = False
+    moveUp = False
+    moveDown = False
+ 
+    MOVESPEED = 1
+    while True:
+    # Check for events.
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            #updates cube onto mouse position
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                player.center = pygame.mouse.get_pos()
+                print(pygame.mouse.get_pos())
+ 
+        if player.bottom < WINDOWHEIGHT:
+            player.y += 1
+    # Draw the white background onto the surface.
+        windowSurface.fill((255, 255, 255))
+ 
+        # Draw the player onto the surface.
+        pygame.draw.rect(windowSurface, (0, 0, 0), player)
+        # Draw the window onto the screen.
+        pygame.display.update()
+        mainClock.tick(120)
+ 
+if __name__ == "__main__":
+    movements()
