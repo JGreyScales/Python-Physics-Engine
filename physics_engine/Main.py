c95e6e9ac6bc841d914de760585b8bdf95e6a551
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-09-23 20:56:22 -0400

    Update Main.py

--- physics_engine/Main.py
+++ physics_engine/Main.py
@@ -23,6 +23,10 @@ pygame.display.set_caption('Physics engine')
 def gameloop():    
     player = pygame.Rect(WINDOWWIDTH//2, WINDOWHEIGHT//2, 20, 20)
 
+    #acceleration
+    acceleration_count = 0
+    acceleration_list = []
+    acceleration = 0
 
     #displacement
     current_pos = player.center
@@ -33,10 +37,13 @@ def gameloop():
     #gravity
     deltaTime = 0.0
     gravityAcceleration = -9.81 / 3
+    velocity = 0
 
     font = pygame.font.Font('freesansbold.ttf', 15)
-    screens = ['Time_delta', 'Gravity Acceleration', 'Displacement']
+    screens = ['Time_delta', 'Gravity Acceleration', 'Displacement', 'acceleration']
     screens[2] = [font.render('Displacement between frames:' +str(displacement), True, (0,0,0)), (0,30)]
+    screens[3] = [font.render(f'Average Velocity is: {acceleration}pixels/5seconds', True, (128,45,45)), (0,45)]
+
 
 
 
@@ -44,10 +51,13 @@ def gameloop():
     getTicksLastFrame = pygame.time.get_ticks()
 
     while True:
+        
     # Check for events.
         for event in pygame.event.get():
 
-
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
             #updates cube onto mouse position
             if event.type == pygame.MOUSEBUTTONDOWN:
                 player.center = pygame.mouse.get_pos()
@@ -55,12 +65,15 @@ def gameloop():
 
         # very basic gravity script
         if player.bottom < WINDOWHEIGHT:
-            player.y -= gravityAcceleration
+            velocity -= gravityAcceleration * deltaTime
+            player.y += velocity
 
         t = pygame.time.get_ticks()
         deltaTime = (t - getTicksLastFrame) / 1000.0
         getTicksLastFrame = t
         tick_count_displacement += deltaTime
+        acceleration_count += deltaTime
+
 
 
 
@@ -71,8 +84,25 @@ def gameloop():
         # calculates the displacement between frames
         current_pos = player.center
         displacement = (current_pos[0] - old_pos[0]) + (current_pos[1] - old_pos[1])
+        if displacement < 0:
+            acceleration_list.append(abs(displacement))
+        else:
+            acceleration_list.append(displacement)
         old_pos = current_pos
-        
+
+
+        # every 2.5 seconds will update the average accelearation the physiscs object has moved in pixels
+        if acceleration_count > 2.5:
+            for displacement in acceleration_list:
+                acceleration += displacement
+            acceleration /= len(acceleration_list)
+            acceleration = round(acceleration /  5, 2)
+
+            screens[3] = [font.render(f'Average Acceleration is: {acceleration}pixels/2.5seconds', True, (128,0,0)), (0,45)]
+
+            acceleration_list = []
+            acceleration_count = 0
+
         # Draw the player onto the surface.
         pygame.draw.rect(windowSurface, (0, 0, 0), player)
         screens[0] = [font.render('Delta Time:' + str(deltaTime), True, (0,0,0)), (0,0)]
@@ -80,7 +110,9 @@ def gameloop():
 
         if displacement > 3 or displacement < 0 or tick_count_displacement > 0.35:
             screens[2] = [font.render('Displacement between frames:' +str(displacement), True, (0,0,0)), (0,30)]
+
             tick_count_displacement = 0
+
 
         for i in range(len(screens)):
             windowSurface.blit(screens[i][0], screens[i][1])
