050c1f443c3ffddfd3d0b54bd1ab725bc2294d84
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-09-24 21:40:59 -0400

    update() upodate()()()(*)))()

--- physics_engine - Copy/Assets/forumulas.py
+++ physics_engine - Copy/Assets/forumulas.py
@@ -3,10 +3,10 @@ class formulas:
     def __init__(self) -> None:
         pass
 
-    # returns an inverse of i
     def inverse(i):
         return i - (i * 2)
 
     ## calculates and returns terminal velocity 
     def calculate_terminal_velocity(self, m, g, c, p, a): #Mass, Gravity, Coefficent, Density of air, Projected area
         return math.sqrt((2*m*g)/(c*p*a))
+a = formulas
