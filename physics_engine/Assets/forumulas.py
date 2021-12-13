d8087e04558044eea4366d252e6e96a3e54aaae2
Hudson Bolduc <>
2021-12-13 15:09:26 +0000

    stuff sorta went wrong :( :(

--- physics_engine/Assets/forumulas.py
+++ physics_engine/Assets/forumulas.py
@@ -15,3 +15,14 @@ class formulas:
     ## calculates and returns terminal velocity 
     def calculate_terminal_velocity(self, m, g, c, p, a): #Mass, Gravity, Coefficent, Density of air, Projected area
         return math.sqrt((2*m*g)/(c*p*a))
+
+        
+    def vector_angle(I_dont_want_to_fix_this, current_pos, *args):
+      old_pos = args[0]
+      if old_pos == current_pos: return 0
+      print(current_pos, old_pos)
+      try:
+        slope = (current_pos[1] - current_pos[0]) / (old_pos[1] - old_pos[0])
+        theta = abs(math.atan(slope) - 1) * 100
+        print(theta)
+      except(ZeroDivisionError): return 0
