ac7c4f9f3813cfc197d3f32d3f963c99f34ed48d
SaltysBleach <95695470+SaltysBleach@users.noreply.github.com>
2022-01-21 08:17:18 -0500

    Add files via upload
    Fixed input issues

--- physics_engine/Main.py
+++ physics_engine/Main.py
@@ -68,7 +68,7 @@ def gameloop():
         mass, gravity_acceleration, 1.05, air_density, projected_area)
 
     #font to use for entire game
-    key_strokes = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_PERIOD]
+    key_strokes = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_0, pygame.K_PERIOD]
     font = pygame.font.Font('freesansbold.ttf', 15)
     button_font = pygame.font.Font('freesansbold.ttf', 10)
 
@@ -149,7 +149,7 @@ def gameloop():
                         for key in key_strokes:
                             if event.key == key: 
                                 if key == pygame.K_PERIOD: user_text += '.'
-                                else: user_text += str(key_strokes.index(key))
+                                else: user_text += str(key_strokes.index(key+1))
                                 break
 
 
